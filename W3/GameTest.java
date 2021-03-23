1ab0fa80c9a65f4cef1351fded7a5e3a69bf6dda
oceanorizqi <80698013+oceanorizqi@users.noreply.github.com>
2021-03-23 13:01:09 +0700

    Add files via upload

---
+++ W3/GameTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ngoding.issagame;
+
+/**
+ *
+ * @author asus
+ */
+public class GameTest {
+    public static void main(String[] args){
+        GamePlayer demo = new GamePlayer();
+        demo.SetPosition ( 67, 76);
+        demo.runnn(75);
+        demo.runnn();
+    }
+}
